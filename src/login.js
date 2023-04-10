7b7114a07c7901820d6eb4c9bc91e704931afb25
Egor Zaitsev <egor.zaitsev84@gmail.com>
2023-04-10 13:40:21 +0300

    added login screen

--- src/login.js
+++ src/login.js
@@ -1,7 +1,12 @@
 /*
-    <div class="wrapper">
-        <h1 class="title">Выберите ваше устройство</h1>
-
+    <div class="app">
+        <div class="wrapper">
+            <h1 class="title">Выберите ваше устройство</h1>
+            <div class="device">
+                <button class="device__button">computer</button>
+                <button class="device__button">phone</button>
+            </div>
+        </div>
     </div>
 
 */
@@ -11,12 +16,35 @@ function loginScreenRender() {
     app.innerHTML = '';
 
     const wrapper = document.createElement('div');
-    wrapper.classList.add('wrapper');
+    wrapper.classList.add('login__wrapper');
 
     const title = document.createElement('h1');
-    title.classList.add('title');
+    title.classList.add('login__title');
     title.textContent = 'Выберите ваше устройство';
+
+    
 
     app.appendChild(wrapper);
     wrapper.appendChild(title);
+    
+
+    deviceBlockRender(wrapper)
+}
+
+function deviceBlockRender(container) {
+    const deviceChoose = document.createElement('div');
+    deviceChoose.classList.add('login__device');
+
+    const buttonComputer = document.createElement('button');
+    buttonComputer.classList.add('login__device__button');
+    buttonComputer.innerHTML = '<i class="fa-solid fa-mobile fa-2xl" style="color: white;"></i>';
+
+    const buttonPhone = document.createElement('button');
+    buttonPhone.classList.add('login__device__button');
+    buttonPhone.style.textAlign = 'center';
+    buttonPhone.innerHTML = '<i class="fa-solid fa-computer fa-2xl" style="color:white"></i>'
+
+    container.appendChild(deviceChoose);
+    deviceChoose.appendChild(buttonComputer);
+    deviceChoose.appendChild(buttonPhone);
 }
--- src/script.js
+++ src/script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     window.application.screens['login'] = loginScreenRender;
+    window.application.blocks['login'] = deviceBlockRender;
     window.application.renderScreen('login');
+
+    
+
 });
